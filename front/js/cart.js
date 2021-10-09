8e72b5d1122f8e4834d1458dfe7ae4f8f411ef8b
Kevin <kevin.brun.dev@gmail.com>
2021-10-09 14:11:17 +0200

    product.js ok
    avancement cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,1 +1,52 @@
 console.log(localStorage)
+
+let article = document.getElementById("cart__items").innerHTML = 
+  `<article class="cart__item" data-id="${produitId}">
+  <div class="cart__item__img">
+    <img src="${produitImage}" alt="${produitAlt}">
+  </div>
+  <div class="cart__item__content">
+    <div class="cart__item__content__titlePrice">
+      <h2>${produitNom}</h2>
+      <p>${produitPrix} €</p>
+    </div>
+    <div class="cart__item__content__settings">
+      <div class="cart__item__content__settings__quantity">
+        <p>Qté : ${produitQuantity} </p>
+        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${produitQuantity}">
+      </div>
+      <div class="cart__item__content__settings__delete">
+        <p class="deleteItem">Supprimer</p>
+      </div>
+    </div>
+  </div>
+</article>`
+;
+
+// récupérer les produit du local storage
+
+let panier = JSON.parse(localStorage.getItem('cart'));
+console.log(panier)
+
+
+
+
+//recuperer les produits du panier 
+panier.forEach(item => {
+    let produitId = item.id;
+    let produitColor = item.color;
+    let produitQuantity = item.qty;
+    
+    fetch(`http://localhost:3000/api/products/${produitId}`)
+    .then(res => res.json())
+    .then( produitApi => {
+        let produitImage = produitApi.imageUrl
+        let produitAlt = produitApi.altTxt
+        let produitNom = produitApi.name
+        let produitPrix = produitApi.price
+       
+      
+    })
+    
+})
+
--- front/js/product.js
+++ front/js/product.js
@@ -3,41 +3,25 @@ var str = window.location.href;
 var url = new URL(str)
 var recherche_param = new URLSearchParams(url.search);
 if(recherche_param.has('id')) {
-    var id = recherche_param.get('id');
-    console.log(id);
+    var produitUrlId = recherche_param.get('id');
+    console.log(produitUrlId);
 }
 
 /* affichage du produit choisis */ 
-const myFetch = fetch("http://localhost:3000/api/products");
-
+myFetch = fetch(`http://localhost:3000/api/products/${produitUrlId}`);
 myFetch.then(res => res.json())
-        .then(data => {
-            for (let index = 0; index < data.length; index++) {
-                const produit = data[index];
-                if (produit._id == id) {
+        .then(produit => {
                     document.querySelector(".item__img").innerHTML =`<img src="${produit.imageUrl}" alt="${produit.altTxt}">`;
-                    document.getElementById("title").innerHTML = `${produit.name}`;
-                    document.getElementById("price").innerHTML = `${produit.price}`;
-                    document.getElementById("description").innerHTML = `${produit.description}`;  
-                    for (let color = 0; color < produit.colors.length; color++) {
-                    const couleur = produit.colors[color];
-                    document.getElementById("colors").innerHTML += `<option value="${couleur}">${couleur}</option>`;
-                    }       
-                }
-            }        
-        }   
+                    document.getElementById("title").innerHTML = produit.name;
+                    document.getElementById("price").innerHTML = produit.price;
+                    document.getElementById("description").innerHTML = produit.description;  
+                    produit.colors.forEach(couleur => {
+                        document.getElementById("colors").innerHTML += `<option value="${couleur}">${couleur}</option>`;
+                    });
+                }       
     )
-/*envois produit vers cart*/
-/*let bouton = document.getElementById('addToCart')
-bouton("click", function(){
-    fetch("http://localhost:3000/api/products"),{
-        "method":"POST",
-        Headers:{
-            'Content-Type': 'application/json'
-        },
-        'body': JSON.stringify({}) 
-    }
-})*/
+
+
 let bouton = document.getElementById('addToCart');
 function couleur(_selectId='colors'){
     let couleur = document.getElementById('colors');
@@ -48,21 +32,33 @@ function nombre() {
 
     return nombre;
 }
-/*let nombre = document.getElementsById('quantity').value;*/
 bouton.addEventListener("click", function(){
-    localStorage.setItem("id", id)
-    localStorage.setItem("couleur", couleur())
-    localStorage.setItem('nombre', nombre())
-    console.log(localStorage)
-    fetch("http://localhost:3000/api/products"),{
-        method : "POST",
-        Headers : {
-            "Content-Type" : "application/json"
-        },
-        body: JSON.stringify
+    const newItem = {
+        id: produitUrlId,
+        color: couleur(),
+        qty: nombre(),
+    }
+    // S'il y a du monde dans le panier
+    if(localStorage.getItem('cart') && localStorage.getItem('cart').length > 0) {
+        const cart = JSON.parse(localStorage.getItem('cart'));
+        // Je recherche la position dans l'array du produit que je veux ajouter. S'il n'existe pas, sa valeur sera de "-1"
+        const productPosition = cart.findIndex(item => item.id === newItem.id && item.color === newItem.color);
+        // Donc, si mon produit n'est pas déjà dans le panier
+        if(productPosition === -1) {
+            cart.push(newItem);
+            localStorage.setItem('cart', JSON.stringify(cart));
+        } else {
+            // Si le produit est déjà dans le panier, je met à jour sa quantité via sa position dans l'array
+            cart[productPosition].qty = parseInt(cart[productPosition].qty) + parseInt(newItem.qty);
+            localStorage.setItem('cart', JSON.stringify(cart));
+        }
+    } else {
+        let newCart = new Array();
+        newCart.push(newItem);
+        localStorage.setItem('cart', JSON.stringify(newCart));
     }
 })
-
+console.log(localStorage)
 
 
 
