3e893711fec7d1e8f7f31261b5c270c6d120f2d7
Kevin <Kevin.Brun.Dev@gmail.com>
2021-10-09 15:04:53 +0200

    affichage panier OK

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,27 +1,5 @@
 console.log(localStorage)
 
-let article = document.getElementById("cart__items").innerHTML = 
-  `<article class="cart__item" data-id="${produitId}">
-  <div class="cart__item__img">
-    <img src="${produitImage}" alt="${produitAlt}">
-  </div>
-  <div class="cart__item__content">
-    <div class="cart__item__content__titlePrice">
-      <h2>${produitNom}</h2>
-      <p>${produitPrix} €</p>
-    </div>
-    <div class="cart__item__content__settings">
-      <div class="cart__item__content__settings__quantity">
-        <p>Qté : ${produitQuantity} </p>
-        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${produitQuantity}">
-      </div>
-      <div class="cart__item__content__settings__delete">
-        <p class="deleteItem">Supprimer</p>
-      </div>
-    </div>
-  </div>
-</article>`
-;
 
 // récupérer les produit du local storage
 
@@ -32,11 +10,12 @@ console.log(panier)
 
 
 //recuperer les produits du panier 
-panier.forEach(item => {
+for (const produit in panier) {
+  if (Object.hasOwnProperty.call(panier, produit)) {
+    const item = panier[produit];
     let produitId = item.id;
     let produitColor = item.color;
     let produitQuantity = item.qty;
-    
     fetch(`http://localhost:3000/api/products/${produitId}`)
     .then(res => res.json())
     .then( produitApi => {
@@ -44,9 +23,32 @@ panier.forEach(item => {
         let produitAlt = produitApi.altTxt
         let produitNom = produitApi.name
         let produitPrix = produitApi.price
-       
+            
+            document.getElementById("cart__items").innerHTML += 
+              `<article class="cart__item" data-id="${produitId}">
+              <div class="cart__item__img">
+                <img src="${produitImage}" alt="${produitAlt}">
+              </div>
+              <div class="cart__item__content">
+                <div class="cart__item__content__titlePrice">
+                  <h2>${produitNom}</h2>
+                  <p>${produitPrix} €</p>
+                </div>
+                <div class="cart__item__content__settings">
+                  <div class="cart__item__content__settings__quantity">
+                    <p>Qté : ${produitQuantity} </p>
+                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${produitQuantity}">
+                  </div>
+                  <div class="cart__item__content__settings__delete">
+                    <p class="deleteItem">Supprimer</p>
+                  </div>
+                </div>
+              </div>
+            </article>`
+          }
+        )
       
-    })
+    }
     
-})
+}
 
