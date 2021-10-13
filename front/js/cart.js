8036ccc84b45b011383f2586c4441988a87b5447
Kevin <kevin.brun.dev@gmail.com>
2021-10-13 20:16:17 +0200

    avancée cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,97 +1,74 @@
 console.log(localStorage)
 
-
 // récupérer les produit du local storage
-
 let panier = JSON.parse(localStorage.getItem('cart'));
 console.log(panier)
-
-let prixTotal = new Array()
-
-
-let quantiteTotal = new Array()
-
-
-
+let prixTotal = 0
+let quantiteTotal = 0
 
 //recuperer les produits du panier et les affichées
-for (const produit in panier) {
-  
-  if (Object.hasOwnProperty.call(panier, produit)) {
-    const item = panier[produit];
-    let produitId = item.id;
-    let produitColor = item.color;
-    let produitQuantity = item.qty;
+for (const produit of panier) {
 
+    let produitId = produit.id;
+    let produitColor = produit.color;
+    let produitQuantity = produit.qty;   
 
-
-    
     fetch(`http://localhost:3000/api/products/${produitId}`)
     .then(res => res.json())
     .then( produitApi => {
+
         let produitImage = produitApi.imageUrl
         let produitAlt = produitApi.altTxt
         let produitNom = produitApi.name
         let produitPrix = produitApi.price
         let total = produitPrix * produitQuantity
-            
-            document.getElementById("cart__items").innerHTML += 
-              `<article class="cart__item" data-id="${produitId}">
-                <div class="cart__item__img">
-                  <img src="${produitImage}" alt="${produitAlt}">
-                </div>
-                <div class="cart__item__content">
-                  <div class="cart__item__content__titlePrice">
-                    <h2>${produitNom}</h2>
-                    <p>${produitColor}</p>
-                    <p data-name="prix" id="prix">${total} €</p>
-                  </div>
-                  <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                      <p id="quantite">Qté : ${produitQuantity} </p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${produitQuantity}">
-                    </div>
-                    <div class="cart__item__content__settings__delete">
-                      <p class="deleteItem">Supprimer</p>
-                    </div>
-                  </div>
-                </div>
-              </article>`  
 
-              
+        let newItem = `<article class="cart__item item_${produitId}" data-id="${produitId}">
+    <div class="cart__item__img">
+      <img src="${produitImage}" alt="${produitAlt}">
+    </div>
+    <div class="cart__item__content">
+      <div class="cart__item__content__titlePrice">
+        <h2>${produitNom}</h2>
+        <p>${produitColor}</p>
+        <p data-name="prix" id="prix">${total} €</p>
+      </div>
+      <div class="cart__item__content__settings">
+        <div class="cart__item__content__settings__quantity">
+          <p id="quantite">Qté : ${produitQuantity} </p>
+          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${produitQuantity}">
+        </div>
+        <div class="cart__item__content__settings__delete">
+          <p class="deleteItem">Supprimer</p>
+        </div>
+      </div>
+    </div>
+    </article>`    
 
-
-              //prix total  
-              let ajoutTableauTotal = prixTotal.push(total)
-              const reducer = (previousValue, currentValue) => previousValue + currentValue;
-              let prixFinal = prixTotal.reduce(reducer)
-              document.getElementById('totalPrice').innerHTML = prixFinal
-
-              //quantité totale
-
-              let ajoutQuantiteTotale = quantiteTotal.push(produitQuantity);
-              const compte = (previousValue, currentValue) => parseInt(previousValue) + parseInt(currentValue);
-              let quantiteFinale = quantiteTotal.reduce(compte)
-              document.getElementById('totalQuantity').innerHTML = quantiteFinale
-              
-
-              // suppression article
-              
+        document.getElementById('cart__items').insertAdjacentHTML('afterbegin', newItem);
+          //prix total  
+              prixTotal= parseInt(prixTotal) + parseInt(total)
+              document.getElementById('totalPrice').innerHTML = prixTotal
+          //quantité totale
+              quantiteTotal = parseInt(quantiteTotal) + parseInt(produitQuantity)
+              document.getElementById('totalQuantity').innerHTML = quantiteTotal
+          // suppression article
+              let supprItem = document.querySelector('.deleteItem')
+              console.log(supprItem)  
+              let article = document.querySelector(`.item_${produitId}`)
+              let articleId = article.dataset.id
+              console.log(articleId)
+              let pos = panier.findIndex(item => item.id === articleId && item.color === produitColor)
+              console.log(pos)
+              let removedItem =  panier[pos]
+                   console.log(removedItem)
+              supprItem.addEventListener('click', function(){      
+                   localStorage.removeItem('removedItem')                  
+                })
             }
-        ) 
-     }  
+        )  
 }
 
-let suppr = document.getElementsByClassName('deleteItem')  
-console.log(suppr)
 
-for (const produit in panier) {
-  if (Object.hasOwnProperty.call(panier, produit)) {
-    const produitId = panier[produit].id;
-    console.log(produitId)
-    
-    let article = document.querySelector('#cart__items')
-    console.log(article)
 
-  }
-}
+
